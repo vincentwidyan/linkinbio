5baf6aa9dcc20a5907e2dd24f88d1772c8f84761
Vincentius <vincentirine@Vincentiuss-MacBook-Air.local>
2024-09-12 00:55:17 +0700

    put all file  commit

---
+++ data/BioData.js
@@ -0,0 +1,27 @@
+// date: 1 Aug, 2022
+const bioData = [
+    {
+        name: '@vincentwidyan',
+        username: '',
+        url: 'https://nxtlnk.vercel.app',
+        titleImg: false,
+        avatar: '/avatar.png',
+        nftAvatar: false,
+        description: '❣️ Love creating visually appealing spreadsheet',
+        descShow: true,
+        subdesc: 'Follow for more!',
+        subdescShow: true,
+        newProductUrl: 'https://erindatastudio.gumroad.com/l/alcgij',
+        newProduct: true,
+        freelanceUrl: 'https://www.upwork.com/freelancers/~01eb071ae0a15f03f8',
+        freelance: true,
+        author: 'realvjy',
+        authorURL: 'https://twitter.com/realvjy',
+        footerText: 'template by'
+    },
+];
+
+export default bioData;
+
+
+
---
+++ data/LinksData.js
@@ -0,0 +1,173 @@
+// date 29-Jul, 2022
+const webLinks = [
+    // All social profile
+    {
+        title: 'Twitter',
+        url: 'https://twitter.com/realvjy',
+        type: 'social',
+        icon: '/twitter.svg',
+        on: false
+    },
+    {
+        title: 'Instagram',
+        url: 'https://www.instagram.com/erindatastudio/',
+        type: 'social',
+        icon: '/insta.svg',
+        on: true
+    },
+    {
+        title: 'Dribbble',
+        url: 'https://dribbble.com/realvjy',
+        type: 'social',
+        icon: '/dribbble.svg',
+        on: false
+    },
+    {
+        title: 'Medium',
+        url: 'https://medium.com/@realvjy',
+        type: 'social',
+        icon: '/medium.svg',
+        on: false
+    },
+    {
+        title: 'Github',
+        url: 'https://github.com/vincentwidyan',
+        type: 'social',
+        icon: '/github.svg',
+        on: true
+    },
+    {
+        title: 'Pinterest',
+        url: 'https://id.pinterest.com/vincentwwidyan',
+        type: 'social',
+        icon: '/pinterest.svg',
+        on: true
+    },
+    {
+        title: 'Youtube',
+        url: 'https://www.youtube.com/@vincentwidyan',
+        type: 'social',
+        icon: '/youtube.svg',
+        on: true
+    },
+    {
+        title: 'Figma',
+        url: 'https://figma.com/@realvjy',
+        type: 'social',
+        icon: '/figma.svg',
+        on: false
+    },
+    {
+        title: 'Behance',
+        url: 'https://behance.net/realvjy',
+        type: 'social',
+        icon: '/behance.svg',
+        on: false
+    },
+    {
+        title: 'Linkedin',
+        url: 'https://www.linkedin.com/in/realvjy/',
+        type: 'social',
+        icon: '/linkedin.svg',
+        on: false
+    },
+    {
+        title: 'Mastodon',
+        url: 'https://mastodon.online/@realvjy',
+        type: 'social',
+        icon: '/mastodon.svg',
+        on: false
+    },
+    {
+        title: 'Hive Social',
+        url: 'https://www.hivesocial.app/?@realvjy',
+        type: 'social',
+        icon: '/hive.svg',
+        on: false
+    },
+    {
+        title: 'Post',
+        url: 'https://www.post.news/realvjy',
+        type: 'social',
+        icon: '/post.svg',
+        on: false
+    },
+    {
+        title: 'foundation',
+        url: 'https://vjy.me',
+        type: 'nft',
+        icon: '/foundation.svg',
+        on: false
+    },
+    {
+        title: 'opensea',
+        url: 'https://vjy.me',
+        type: 'nft',
+        icon: '/opensea.svg',
+        on: false
+    },
+    {
+        title: 'new collection',
+        url: 'https://vjy.me',
+        type: 'nft',
+        icon: '/opensea.svg',
+        on: false
+    },
+    // featured section
+    {
+        title: 'Gumroad Store',
+        url: 'https://erindatastudio.gumroad.com/',
+        type: 'featured',
+        icon: '/gumroad.png',
+        on: true
+    },
+    {
+        title: 'Blog (soon)',
+        url: 'https://github.com/vincentwidyan',
+        type: 'featured',
+        icon: '/blog.png',
+        on: true
+    },
+    {
+        title: 'Documentation',
+        url: 'https://github.com/realvjy/nxt-lnk#nxt-lnk',
+        type: 'featured',
+        icon: '/doc.png',
+        on: false
+    },
+    {
+        title: 'Github',
+        url: 'https://github.com/vincentwidyan',
+        type: 'featured',
+        icon: '/github-fill.svg',
+        on: true
+    },
+
+    {
+        title: 'Buy Me a Coffee',
+        url: 'https://www.buymeacoffee.com/realvjy',
+        type: 'other',
+        icon: '/bmc.png',
+        on: true
+    },
+
+    {
+        title: 'link demo',
+        url: 'https://vjy.me/lnk',
+        type: 'other',
+        icon: '/vjy.png',
+        on: false
+    },
+
+    {
+        title: 'maker website',
+        url: 'https://vjy.me',
+        type: 'other',
+        icon: '/vjy.png',
+        on: false
+    },
+];
+
+export default webLinks;
+
+
